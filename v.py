12b51fb87ee582d8052069036740ad30e20b3c2e
Filmsaga00 <filmsaga00+2642@gmail.com>
2024-11-30 10:59:34 +0530

    Update v.py

--- v.py
+++ v.py
@@ -31,9 +31,9 @@ except Exception as e:
     
 TOKEN = '7643617660:AAFQa6EotE_K8rk7wUWAVrm1zb-ZzF81lrA'
 MONGO_URI = 'mongodb+srv://VIKASH:BadnamBadshah@cluster0.jv9he.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -
-CHANNEL_ID = -
-error_channel_id = -
+FORWARD_CHANNEL_ID = -1002461787556
+CHANNEL_ID = -1002461787556
+error_channel_id = -1002461787556
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
